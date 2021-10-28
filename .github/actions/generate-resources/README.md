24139a486d14500e57c51ba918d13fe07cb5be35
Greg <gjsyme@gmail.com>
2021-10-28 00:49:21 -0500

    Create demo action and workflow for RESOURCES.md
    There's a chance this won't clear existing data each time; otherwise
    pulls airtable data and turns it into functional markdown as advertised

---
+++ .github/actions/generate-resources/README.md
@@ -0,0 +1,17 @@
+# Generate RESOURCES.md Action
+
+This action reads data from the Developer DAO Airtable and puts its content into RESOURCES.md
+
+## Inputs
+
+None
+
+## Outputs
+
+### resourceText
+
+The markdown text that has been generated (to be written to the file)
+
+## Example Usage
+
+uses: actions/generate-resources@v0.1.x
---
+++ .github/actions/generate-resources/main.js
@@ -0,0 +1,91 @@
+const Airtable = require('airtable');
+const fs = require('fs');
+
+// airtable resources managed by @kempsterrrr
+const AIRTABLE_READONLY_KEY = 'keyDaaDlZelNrerXQ';
+const AIRTABLE_RESOURCE_BASE = 'appPP6MpO5hrfQwqI';
+
+// copy pasta from the old README.md
+const RESOURCE_HEADER = `# Resources
+
+Welcome to the [DeveloperDAO](https://github.com/Developer-DAO/developer-dao) **Knowledge Base**.
+
+The community has created this knowledge base to help you **learn** and **grow** in your Web3 journey, whether you want to start learning about Web3, or you're building your first dApp, or you're deep into the world of solidity.
+
+## Terminology
+
+- Visit the [Glossary](GLOSSARY.md) to understand more about a specific term.
+
+## Resources
+
+`;
+
+const airtable = new Airtable({ apiKey: AIRTABLE_READONLY_KEY });
+
+let completed = { resources: false, authors: false};
+
+let resourceList = [];
+airtable.base(AIRTABLE_RESOURCE_BASE)('Resources')
+  .select()
+  .eachPage((records, fetchNextPage) => {
+    try{
+      resourceList = records ? resourceList.concat(records.map(item => item._rawJson)) : resourceList;
+      fetchNextPage();
+    }catch(e){
+      console.error('error fetching resources',e);
+    }
+  },
+  (err) => {
+    if(err){
+      console.error('error fetching Resources',err);
+    }
+    completed.resources = true;
+    tryProcessData();
+  });
+
+let authorList = [];
+airtable.base(AIRTABLE_RESOURCE_BASE)('Authors')
+  .select({})
+  .eachPage(
+    (records, fetchNextPage) => {
+      try{
+        authorList = records ? authorList.concat(
+          records.map((item) => item._rawJson),
+        ) : authorList;
+        fetchNextPage();
+      }catch(e){
+        console.error('error fetching authors',e);
+      }
+    },
+    (err) => {
+      if (err) {
+        console.error('error fetching Authors', err);
+      }
+      completed.authors = true;
+      tryProcessData();
+    },
+  );
+
+const tryProcessData = () => {
+  if(completed.authors && completed.resources){
+    processData();
+  }
+}
+
+const processData = () => {
+  // console.log('authors',authorList);
+  // console.log('resources',resourceList);
+  const authorMap = {};
+  // easy to look up from resource author list
+  authorList.forEach(item => {
+    authorMap[item.id] = item.fields;
+  });
+
+  const resourceString = resourceList.map(item => {
+    // convert it to a formatted string
+    return `- [${item.fields.Title}](${item.fields.Source})\n\n  Author${item.fields.Author?.length>1 ? 's' : ''}: ${item.fields.Author?.map(authorId => authorMap[authorId].Name)}${item.fields.Summary ? '\n'+item.fields.Summary : ''}` 
+  }).join('\n\n');
+
+  let returnString = RESOURCE_HEADER + resourceString;
+  fs.writeFileSync('./RESOURCES.md', returnString);
+}
